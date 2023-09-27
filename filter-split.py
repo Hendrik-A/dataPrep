601fe3e62ae427508702c8944ced304382cfd710
Hendrik-A <145358341+Hendrik-A@users.noreply.github.com>
2023-09-27 17:50:16 +0200

    Update filter-split.py rm log, drop token cols, order by rand still missing

--- filter-split.py
+++ filter-split.py
@@ -62,10 +62,6 @@ def main():
   sc = pyspark.SparkContext(conf=conf)
   spark = pyspark.sql.SparkSession(sc)
 
-  log_dir = os.path.join(args.data_root, "logging")
-  if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-  log_file = os.path.join(log_dir, "log.txt")
   b_keywords = sc.broadcast(KEYWORDS)
   
   data_path = os.path.join(args.data_root, 'countedTokens.txt')
@@ -75,20 +71,9 @@ def main():
   df = df.where(F.col('PXtokens') <= 16384)
   df = df.withColumn('match', section_match(b_keywords)('section_names'))
   df = df.filter(df.match == True)
-  df = df.orderBy(F.col('LEDtokens'), F.col('PXtokens'), ascending=False).limit(5000)
-  
-  with open(log_file, "a+") as writer:
-    writer.write("------Unsplitted data statistics------\n")
-    writer.write("Total entries:"+str(df.count())+"\n")
-    writer.write("avg LED tokens:"+str(df.select(F.avg(df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("min LED tokens:"+str(df.select(F.min(df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("max LED tokens:"+str(df.select(F.max(df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("avg Pegasus-X tokens:"+str(df.select(F.avg(df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("median Pegasus-X tokens:"+str(df.approxQuantile("PXtokens", [0.5], 0))+"\n")
-    writer.write("min Pegasus-X tokens:"+str(df.select(F.min(df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("max Pegasus-X tokens:"+str(df.select(F.max(df['PXtokens'])).collect()[0][0])+"\n")
+  df = df.orderBy(F.col('LEDtokens'), F.col('PXtokens'), ascending=False).limit(5000).drop("LEDtokens", "PXtokens")
+    
+  # order by rand() missing
   
   rows = df.count()
 
@@ -97,46 +82,6 @@ def main():
   valid_df = valid_test_df.limit(round(valid_test_df.count()*0.5))
   test_df = valid_test_df.filter(~valid_test_df["article_id"].isin(list(valid_df.select(train_df.article_id).toPandas()['article_id'])))
 
-  with open(log_file, "a+") as writer:
-    writer.write("------Train statistics------\n")
-    writer.write("Total entries:"+str(str(train_df.count()))+"\n")
-    writer.write("avg LED tokens:"+str(train_df.select(F.avg(train_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(train_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("min LED tokens:"+str(train_df.select(F.min(train_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("max LED tokens:"+str(train_df.select(F.max(train_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("avg Pegasus-X tokens:"+str(train_df.select(F.avg(train_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(train_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("median Pegasus-X tokens:"+str(train_df.approxQuantile("PXtokens", [0.5], 0))+"\n")
-    writer.write("min Pegasus-X tokens:"+str(train_df.select(F.min(train_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("max Pegasus-X tokens:"+str(train_df.select(F.max(train_df['PXtokens'])).collect()[0][0])+"\n")
-
-    writer.write("------Validation statistics------\n")
-    writer.write("Total entries:" + str(valid_df.count())+"\n")
-    writer.write("avg LED tokens:"+str(valid_df.select(F.avg(valid_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(valid_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("min LED tokens:"+str(valid_df.select(F.min(valid_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("max LED tokens:"+str(valid_df.select(F.max(valid_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("avg Pegasus-X tokens:"+str(valid_df.select(F.avg(valid_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(valid_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("median Pegasus-X tokens:"+str(valid_df.approxQuantile("PXtokens", [0.5], 0))+"\n")
-    writer.write("min Pegasus-X tokens:"+str(valid_df.select(F.min(valid_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("max Pegasus-X tokens:"+str(valid_df.select(F.max(valid_df['PXtokens'])).collect()[0][0])+"\n")
-    
-    writer.write("------Test statistics------\n")
-    writer.write("Total entries:"+str(test_df.count()))
-    writer.write("avg LED tokens:"+str(test_df.select(F.avg(test_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(test_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("min LED tokens:"+str(test_df.select(F.min(test_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("max LED tokens:"+str(test_df.select(F.max(test_df['LEDtokens'])).collect()[0][0])+"\n")
-    writer.write("avg Pegasus-X tokens:"+str(test_df.select(F.avg(test_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("median LED tokens:"+str(test_df.approxQuantile("LEDtokens", [0.5], 0))+"\n")
-    writer.write("median Pegasus-X tokens:"+str(test_df.approxQuantile("PXtokens", [0.5], 0))+"\n")
-    writer.write("min Pegasus-X tokens:"+str(test_df.select(F.min(test_df['PXtokens'])).collect()[0][0])+"\n")
-    writer.write("max Pegasus-X tokens:"+str(test_df.select(F.max(test_df['PXtokens'])).collect()[0][0])+"\n")
-
-  train_df.drop("LEDtokens", "PXtokens")
-  test_df.drop("LEDtokens", "PXtokens")
-  valid_df.drop("LEDtokens", "PXtokens")
   train_df.write.json(path=os.path.join(args.data_root, "train"), mode="overwrite")
   test_df.write.json(path=os.path.join(args.data_root, "test"), mode="overwrite")
   valid_df.write.json(path=os.path.join(args.data_root, "val"), mode="overwrite")
